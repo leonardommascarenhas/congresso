a62cf24f5b309ac36096f9b8090e67ff4c0f8dd5
leonardo moura <leonardommascarenhas@gmail.com>
2025-08-06 10:33:51 -0300

    pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import { WhatsAppFloatingButton } from "./WhatsappButton";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Congresso de Odontologia Digital • 6 de setembro • Salvador",
@@ -26,6 +27,31 @@ html {
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
+
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1094142649329229');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1094142649329229&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
       <body className="relative">
         {children}
